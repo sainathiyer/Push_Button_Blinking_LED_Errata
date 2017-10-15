2890a8d90d63f7101050103d10ac07139b4329d5
Sainathan Iyer <sainathaniyer@gmail.com>
2017-10-15 14:25:14 +0530

    Add files via upload

---
+++ Push_Button_Blinking_LED_Errata.ino
@@ -0,0 +1,28 @@
+const int LED = 13;//THE PIN FOR LED
+const int BUTTON = 7;//the pin where the push button is connected
+int val = 0;//val will be used to store the state of the input PIN
+int state = 0;//0=LED OFF while 1=Led ON
+void setup()
+{
+  pinMode(LED, OUTPUT);// tell arduino LED is output
+  pinMode(BUTTON, INPUT);// tell arduino BUTTON is INPUT
+}
+void loop()
+{
+  val = digitalRead(BUTTON);//read input value and store it.
+  if (val == HIGH)//check if input is HIGH(button pressed)
+  {
+    state = 1-state;
+  }
+  if (state == 1)
+  {
+    digitalWrite(LED, HIGH);//turn LED ON
+  }
+  else
+  {
+    digitalWrite(LED, LOW);
+    
+  } 
+}
+
+
